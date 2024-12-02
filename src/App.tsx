50ae71adb1c5dcdbab4e5a3324c7c33d4ee85a17
nsx_69 <mrmanafest16@gmail.com>
2024-12-02 20:47:02 +0500

    Test commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,18 @@ function App() {
       <div className="App">
         <div>
           <h3>What to learn</h3>
+
           <div>
             <input/>
             <button>+</button>
           </div>
+
           <ul>
             <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
             <li><input type="checkbox" checked={true}/> <span>JS</span></li>
             <li><input type="checkbox" checked={false}/> <span>React</span></li>
           </ul>
+
           <div>
             <button>All</button>
             <button>Active</button>
